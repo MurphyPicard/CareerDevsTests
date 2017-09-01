2fd00ae16a98a20d1ad7943db5afaf55ae529baf
Ara <ayaghsizian@gmail.com>
2017-08-31 20:12:43 -0400

    automatically downloads a file with contents

--- app.js
+++ app.js
@@ -3,13 +3,16 @@
 // nafissa(4,6);
 
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(req,res){
-  res.writeHead(200);
-
-  console.log("hi");
-  res.write('Hello there');
+  res.writeHead(200, {"Content-Type":"html/text"});
+  var contents = fs.readFileSync('index.html');
+  console.log(contents);
+  res.write(contents);
+  res.write("This is dog");
   res.end();
+
 }).listen(8080, function(){
   console.log('listening...');
 });
